2f284f8ecf9d7106c034c4f8b0ab016076b6424e
dcyqo <kotfloppa.official@gmail.com>
2026-07-03 23:50:24 +0300

    Исправлен баг с открытием меню

--- murino-script.lua
+++ murino-script.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/dcyqo/Rayfield/refs/heads/main/source.lua"))()
+local Rayfield = loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/dcyqo/Rayfield/refs/heads/main/source.lua"))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Скрипт: Мурино Хоррор",
